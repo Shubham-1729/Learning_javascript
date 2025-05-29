ef8abc02a4385fac04aeac636102cfee8b6cd825
Shubham kumar <141277349+Shubham-1729@users.noreply.github.com>
2025-05-29 17:13:25 +0000

    ConversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "22abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" = NaN
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLoggedIn = "Shubham"
 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,7 +24,30 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log (stringNumber)
-console.log(typeof stringNumber);
+// console.log (stringNumber)
+// console.log(typeof stringNumber);
+
+// ************************** Operations ****************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Shubham"
+let str2 = " Kumar"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+console.log(+true);
+console.log(+"");
+
+
+
+
 
 
