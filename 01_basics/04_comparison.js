7b3470c5b9fba0031296e2872b856ebabce47609
Shubham kumar <141277349+Shubham-1729@users.noreply.github.com>
2025-05-29 17:50:52 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log("2" > 1); // here string 2 is converted to a number
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true cuz in case of comparisons convert null to a number
+
+// === strict check
+console.log("2" == 2)
+console.log("2" === 2);
+
